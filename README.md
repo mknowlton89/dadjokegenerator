47aa7ae11106f8f294350bb953af48e8d39737c3
Michael Knowlton <mknowlton89@gmail.com>
2021-02-07 15:30:45 -0500

    added svgs, updated functions, and build favs page

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # dadjokegenerator
 
 This is a simple app that leverages the icanhazdadjoke open API to generate jokes on-demand!
+
+Copy the joke to your clipboard and share it with others!
+
+Share it with Facebook.
+
+Share it on Instagram.
+
+And favorite it. (The favorites are saved to your browser.)
--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+let favs = [];
 
 const jokey = document.getElementById('joke');
 const get_joke = document.getElementById('get_joke');
@@ -9,13 +10,14 @@ const jokes= await fetch('https://icanhazdadjoke.com/',{
         'Accept':'application/json'
     }
 });
+
     const joke = await jokes.json();
 
     jokey.innerHTML = joke.joke;
 
-    document.getElementById("copy").style.visibility = "visible";
+    document.getElementById("engagement-container").style.visibility = "visible";
 
-    document.getElementById("copy").innerHTML = "Copy to Clipboard"
+    resetState();
 
 }
 
@@ -27,5 +29,63 @@ function CopyToClipboard() {
     document.execCommand("copy");
     window.getSelection().removeAllRanges();// to deselect
 
-    document.getElementById("copy").innerHTML = "Copied!"
+    // document.getElementById("copy").innerHTML = "Copied"
+    document.getElementById("copyicon").src = "svgs/clipboard-check.svg";
+}
+
+function shareToFacebook() {
+    // const range = document.createRange();
+    // range.selectNode(document.getElementById("joke"));
+    // window.getSelection().removeAllRanges(); // clear current selection
+    // window.getSelection().addRange(range); // to select text
+    // document.execCommand("copy");
+    // window.getSelection().removeAllRanges();// to deselect
+
+    // // document.getElementById("copy").innerHTML = "Copied"
+    // document.getElementById("copyicon").src = "svgs/clipboard-check.svg";
+}
+
+function shareToInstagram() {
+    // const range = document.createRange();
+    // range.selectNode(document.getElementById("joke"));
+    // window.getSelection().removeAllRanges(); // clear current selection
+    // window.getSelection().addRange(range); // to select text
+    // document.execCommand("copy");
+    // window.getSelection().removeAllRanges();// to deselect
+
+    // // document.getElementById("copy").innerHTML = "Copied"
+    // document.getElementById("copyicon").src = "svgs/clipboard-check.svg";
+}
+
+function favorite() {
+    document.getElementById("favicon").src = "svgs/star-fill.svg";
+
+    // Get the joke in a string.
+    let joke = document.getElementById("joke").innerHTML;
+
+    // Add joke to array
+    favs.push(joke);
+    // localStorage["favs"] = JSON.stringify(datas);
+
+    console.log(favs);
+
+    localStorage.setItem("favs", favs);
+
+// Retrieve
+// var stored_datas = JSON.parse(localStorage["datas"]);
+
+
+
+}
+
+function resetState() {
+    document.getElementById("copyicon").src = "svgs/clipboard.svg";
+    // document.getElementById("shareFbicon").src = "svgs/facebook.svg";
+    // document.getElementById("shareIgicon").src = "svgs/instagram.svg";
+    document.getElementById("favicon").src = "svgs/star.svg";
+ 
+}
+
+function loadFavs() {
+    alert("loadFavs was called");
 }
