2f1d6d382965f0b52f8cb396f296c2d6a82b9d2b
Michael Knowlton <mknowlton89@gmail.com>
2021-02-16 16:55:12 -0500

    Styling updates

--- scripts.js
+++ scripts.js
@@ -39,6 +39,7 @@ function favorite() {
     const joke = document.getElementById("joke").innerHTML;
     let favs = localStorage.getItem("favs");
 
+    // Check to see if the user has already favorited the joke. If so, we need to remove it from the array and local storage.
     if (favStatus != null) {
         
         // Update the icon to the unfilled star
@@ -109,11 +110,28 @@ function loadFavorites() {
     // Hide the "noFavorites" section.
     document.getElementById("noFavorites").style.visibility = "hidden";
 
+    // // For loop to loop through array and create list.
+    //     for (let i = 0; i < fullFavsList.length; i++) {
+    //         let joke = fullFavsList[i];
+    //         listItem = document.createElement('li');
+    //         listItem.value = i + 1;
+    //         delBtn = document.createElement("button")
+    //         delBtn.innerText = "Delete";
+    //         document.getElementById("favList").appendChild(listItem).innerHTML = joke;
+    //         // document.getElementById("favList").appendChild(delBtn).innerHTML = "Delete"
+    //     }
     // For loop to loop through array and create list.
         for (let i = 0; i < fullFavsList.length; i++) {
             let joke = fullFavsList[i];
-            listItem = document.createElement('li')
-            document.getElementById("favList").appendChild(listItem).innerHTML = joke;
+            // <li><button>Delete</button></li>
+            <li>Test</li>
+
+            // listItem = document.createElement('li');
+            // listItem.value = i + 1;
+            // delBtn = document.createElement("button")
+            // delBtn.innerText = "Delete";
+            // document.getElementById("favList").appendChild(listItem).innerHTML = joke;
+            // document.getElementById("favList").appendChild(delBtn).innerHTML = "Delete"
         }
     }  
 }
@@ -124,4 +142,23 @@ function deleteAll() {
 
     location.reload();
 
+}
+
+function deleteJoke(value) {
+    // TODO - Get the joke the user wants to delete
+    const jokeToDelete = TODO
+
+    // load in favs list from local storage
+    let favslist = localStorage.getItem("favs");
+
+    // Build an array that I can manipulate
+    let fullFavsList = JSON.parse(favslist);
+
+    // Build for loop to find the string in the array that matches the joke the user wants to delete.
+
+    // Remove the joke from the array using slice() or "filter" like in the favorite function.
+
+    // Re-stringify the array and save to local storage
+
+    // Then I need to remove the joke from the list (but ideally, without reloading the page.)
 }
